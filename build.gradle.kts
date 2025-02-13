95016f4568a50e6ca525d9e663f74c8bda28f412
Ruben Gees <rubengees7@gmail.com>
2025-02-13 21:26:06 +0100

    Modernize project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,185 +1,163 @@
-import nu.studer.gradle.jooq.JooqGenerate
 import org.flywaydb.core.Flyway
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jooq.meta.jaxb.Configuration
-import org.jooq.meta.jaxb.MatcherRule
+import org.jooq.codegen.gradle.CodegenTask
 import org.jooq.meta.jaxb.MatcherTransformType
-import org.jooq.meta.jaxb.Matchers
-import org.jooq.meta.jaxb.MatchersEnumType
-import org.jooq.meta.jaxb.MatchersTableType
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
 import org.testcontainers.containers.wait.strategy.WaitAllStrategy
 import org.testcontainers.utility.DockerImageName
-import java.nio.file.Paths
 import java.time.Duration
 
 buildscript {
     dependencies {
-        classpath(platform("org.springframework.boot:spring-boot-dependencies:3.2.3"))
-        classpath("org.testcontainers:postgresql")
-        classpath("org.flywaydb:flyway-core")
-        classpath("org.postgresql:postgresql")
+        classpath(platform(libs.spring.boot.dependencies))
+        classpath(libs.testcontainers.postgresql)
+        classpath(libs.flyway.core)
+        classpath(libs.flyway.postgresql)
+        classpath(libs.postgresql)
     }
 }
 
 plugins {
-    id("org.springframework.boot") version "3.2.3"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
-    id("nu.studer.jooq") version "9.0"
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.jooq.codegen)
 }
 
 group = "org.example"
 version = "0.0.1-SNAPSHOT"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.2.3"))
-    implementation("org.springframework.boot:spring-boot-starter-jooq")
-    implementation("org.flywaydb:flyway-core")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation(platform(libs.spring.boot.dependencies))
+    implementation(libs.spring.boot.starter.jooq)
 
-    jooqGenerator(platform("org.springframework.boot:spring-boot-dependencies:3.2.3"))
-    jooqGenerator("org.postgresql:postgresql")
+    runtimeOnly(libs.postgresql)
 
-    runtimeOnly("org.postgresql:postgresql")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.zonky.test:embedded-database-spring-test:2.5.0")
+    testImplementation(libs.jooq)
+    testImplementation(libs.flyway.core)
+    testImplementation(libs.flyway.postgresql)
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.embedded.database.spring.test)
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
-    }
+kotlin {
+    jvmToolchain(21)
 }
 
-tasks.withType<Test> {
+tasks.test {
     useJUnitPlatform()
 }
 
 jooq {
-    configurations {
-        create("main") {
-            jooqConfiguration.apply {
-                jdbc.apply {
-                    driver = "org.postgresql.Driver"
-                    url = "jdbc:postgresql://localhost:5432/postgres"
-                    user = "postgres"
-                    password = "postgres"
-                }
-                generator.apply {
-                    name = "org.jooq.codegen.KotlinGenerator"
-                    database.apply {
-                        name = "org.jooq.meta.postgres.PostgresDatabase"
-                        inputSchema = "public"
-
-                        isIncludeIndexes = false
-                        isIncludeSequences = false
-                        isIncludeForeignKeys = false
-                        isIncludeUniqueKeys = false
-
-                        generate.apply {
-                            isDaos = true
-                            isImmutablePojos = true
-                            isDeprecationOnUnknownTypes = false
-                            isKotlinNotNullPojoAttributes = true
+    configuration {
+        generator {
+            name = "org.jooq.codegen.KotlinGenerator"
+            database {
+                name = "org.jooq.meta.postgres.PostgresDatabase"
+                inputSchema = "public"
+                excludes = "flyway_.*"
+                schemaVersionProvider =
+                    """
+                        SELECT 'v' || version || '/' || checksum
+                        FROM flyway_schema_history
+                        ORDER BY version DESC LIMIT 1
+                    """.trimIndent()
+            }
+            generate {
+                isDaos = true
+                isImmutablePojos = true
+                isDeprecationOnUnknownTypes = false
+                isKotlinNotNullPojoAttributes = true
+            }
+            target {
+                packageName = "org.example"
+                directory = "build/generated/jooq/main"
+            }
+            strategy {
+                matchers {
+                    enums {
+                        enum_ {
+                            enumClass {
+                                transform = MatcherTransformType.PASCAL
+                                expression = "DB_$0"
+                            }
                         }
-                        target.apply {
-                            packageName = "org.example"
-                            directory = "${layout.buildDirectory.get()}/generated/jooq/main"
-                        }
-                        strategy.apply {
-                            matchers = Matchers().apply {
-                                enums = listOf(
-                                    MatchersEnumType().apply {
-                                        enumClass = MatcherRule().apply {
-                                            transform = MatcherTransformType.PASCAL
-                                            expression = "DB_$0"
-                                        }
-                                    },
-                                )
-                                tables = listOf(
-                                    MatchersTableType().apply {
-                                        pojoClass = MatcherRule().apply {
-                                            transform = MatcherTransformType.PASCAL
-                                            expression = "$0_Pojo"
-                                        }
-                                        tableClass = MatcherRule().apply {
-                                            transform = MatcherTransformType.PASCAL
-                                            expression = "$0_Table"
-                                        }
-                                    },
-                                )
+                    }
+                    tables {
+                        table {
+                            pojoClass {
+                                transform = MatcherTransformType.PASCAL
+                                expression = "$0_Pojo"
+                            }
+                            tableClass {
+                                transform = MatcherTransformType.PASCAL
+                                expression = "$0_Table"
                             }
                         }
                     }
                 }
             }
         }
+
+        jdbc {
+            url = "jdbc:postgresql://localhost:53948/postgres"
+            username = "postgres"
+            password = "postgres"
+        }
+    }
+
+    val imageName = DockerImageName.parse("postgres:17-alpine")
+
+    val container = WaitingForPortPostgresContainer(imageName)
+        .withDatabaseName("postgres")
+        .withUsername("postgres")
+        .withPassword("postgres")
+        .withLogConsumer(Slf4jLogConsumer(logger).withSeparateOutputStreams().withPrefix("postgres"))
+        .apply { portBindings = listOf("53948:${PostgreSQLContainer.POSTGRESQL_PORT}") }
+
+    val jooqCodegenCleanup = tasks.register("jooqCodegenCleanup") {
+        doLast { container.stop() }
+    }
+
+    tasks.withType<CodegenTask> {
+        val schemaLocation = file("src/main/resources/db/migration")
+
+        inputs.dir(schemaLocation)
+
+        doFirst {
+            container.start()
+
+            Flyway
+                .configure()
+                .dataSource(container.jdbcUrl, container.username, container.password)
+                .locations("filesystem:${schemaLocation.absolutePath}")
+                .load()
+                .migrate()
+        }
+
+        doLast { container.stop() }
+
+        finalizedBy(jooqCodegenCleanup)
+    }
+
+    tasks.withType<KotlinCompile> {
+        dependsOn(tasks.withType(CodegenTask::class))
     }
 }
 
+class WaitingForPortPostgresContainer(imageName: DockerImageName) :
+    PostgreSQLContainer<WaitingForPortPostgresContainer>(imageName) {
 
-class KPostgresContainer(imageName: DockerImageName) : PostgreSQLContainer<KPostgresContainer>(imageName) {
     init {
         this.waitStrategy = WaitAllStrategy()
             .withStrategy(this.waitStrategy)
             .withStrategy(HostPortWaitStrategy())
             .withStartupTimeout(Duration.ofSeconds(60))
     }
-}
-
-tasks.withType(JooqGenerate::class.java).configureEach {
-    val container = KPostgresContainer(DockerImageName.parse("postgres:16-alpine"))
-        .withDatabaseName("postgres")
-        .withUsername("postgres")
-        .withPassword("postgres")
-        .withLogConsumer(Slf4jLogConsumer(logger).withSeparateOutputStreams().withPrefix("postgres"))
-
-    val migrationPath = Paths
-        .get(projectDir.path, "src/main/resources/db/migration")
-        .normalize().toAbsolutePath()
-
-    doFirst {
-        container.start()
-
-        Flyway.configure()
-            .dataSource(container.jdbcUrl, container.username, container.password)
-            .locations("filesystem:$migrationPath")
-            .load()
-            .migrate()
-
-        // Hack to get the private jooqConfiguration of the generate task.
-        // Needed to dynamically set jdbc connection parameters.
-        val jooqConfiguration = this::class.java.superclass.getDeclaredField("jooqConfiguration")
-            .apply { isAccessible = true }
-            .get(this) as Configuration
-
-        jooqConfiguration.jdbc.apply {
-            url = container.jdbcUrl
-            username = container.username
-            password = container.password
-        }
-    }
-
-    doLast {
-        container.stop()
-    }
-
-    inputs.files(fileTree(migrationPath))
-        .withPropertyName("migrations")
-        .withPathSensitivity(PathSensitivity.RELATIVE)
-
-    allInputsDeclared.set(true)
 }
